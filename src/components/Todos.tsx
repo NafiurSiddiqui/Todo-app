d761815fcb602e7d70e3610de77f58f9042a37be
saejim <saejim91@gmail.com>
2022-06-13 03:01:22 +0600

    Form componet added for user input

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import Todo from '../Models/todo';
+import TodoItem from './TodoItem';
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
 	return (
 		<ul>
 			{props.items.map((item) => (
-				<li key={item.id}>{item.text}</li>
+				<TodoItem key={item.id} text={item.text} />
 			))}
 		</ul>
 	);
@@ -17,5 +19,6 @@ export default Todos;
  * @FC - gives us the weay to define props type gernerically.
  * -stands for Functional component
  * -- FC is already, generic, we do not need to create generic function.
+ * -- we always set Object here because props are always object with key:value and Children.
  * @Todo - we define our items as Todo object type, which is inside an array. an array full of our list (OBJECT type)
  */
